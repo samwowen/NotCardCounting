16af5c918818738e474f732bf7a8620f5a50006f
Sam Owen <sam.w.owen@gmail.com>
2019-01-16 22:23:44 +0000

    Initial Commit

---
+++ NotCardCountingTests/NotCardCountingTests.swift
@@ -0,0 +1,36 @@
+//
+//  NotCardCountingTests.swift
+//  NotCardCountingTests
+//
+//  Created by Sam Owen on 16/01/2019.
+//  Copyright © 2019 EskiSoftware. All rights reserved.
+//
+
+import XCTest
+@testable import NotCardCounting
+
+class NotCardCountingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
